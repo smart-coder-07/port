cbe8090f1b553e286c3e98c05d3cbb3fa02d8a07
Atul Tiwari <atultiwarikhj@gmail.com>
2023-09-15 15:49:38 +0100

    done

--- port/settings.py
+++ port/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x9iv4muu66u0n@46i%ln_062tc&ll*6@m9ubb^+tfld#4&lcu(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -118,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
@@ -128,3 +127,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
